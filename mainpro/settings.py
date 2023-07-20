4536b2e2f32d32a58cabe66dd43509075c4a75b0
brijesh <brijeshkumarv598@gmail.com>
2023-07-20 21:13:01 +0530

    adding static root

--- mainpro/settings.py
+++ mainpro/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 import os 
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static'),
+# ]
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
