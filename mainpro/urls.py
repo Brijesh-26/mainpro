c5737f20e0625da77ee3eeddc3ac3f1a0e95db81
brijesh <brijeshkumarv598@gmail.com>
2023-07-21 11:12:57 +0530

    updated123

--- mainpro/urls.py
+++ mainpro/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -9,8 +10,7 @@ urlpatterns = [
     path('' , include('authapp.urls')),
     path('blog/', include('mainapp.urls')),
     
-]
-urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 
